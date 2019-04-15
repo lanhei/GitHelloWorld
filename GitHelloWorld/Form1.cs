e8c26727b9d1ad92fd2735b58b38b556f963dae7
lanhei <glay126@126.com>
2019-04-15 23:45:53 +0800

    升级

--- GitHelloWorld/Form1.cs
+++ GitHelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace GitHelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Git HelloWorld V233");
+            MessageBox.Show("Git HelloWorld V4");
         }
     }
 }
